b80ff577b5b017dc500a4f84d78179fca7ec70f1
fdahnoun <fdahnoun@email.unc.edu>
2022-11-05 14:15:36 -0400

    added maps

--- mappin_client/src/App.js
+++ mappin_client/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Login from './components/login';
+import Maps from './components/maps';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Login/ >
     </div>
   );
 }
