90950f67989c68d3d989b54f15c07502f7602666
rahqis <rahqi@ad.unc.edu>
2022-11-05 11:52:00 -0400

    firebase config, del server, created api folder

---
+++ mappin_client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBDFvgudlFjJpimdyw8y3T6DjP012YJww",
+  authDomain: "mappin-68029.firebaseapp.com",
+  projectId: "mappin-68029",
+  storageBucket: "mappin-68029.appspot.com",
+  messagingSenderId: "954536088508",
+  appId: "1:954536088508:web:f1305e79906c7c260151b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
