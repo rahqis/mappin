b590b97f6674c4eab44db07c3ef6334665f269e4
Manaal Iqbal <naz007@email.unc.edu>
2022-11-05 22:00:25 -0400

    binaat changes

--- mappin_client/src/App.js
+++ mappin_client/src/App.js
@@ -3,21 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import  Login from "./components/Login";
 import  Register from "./components/Register";
+import MakeEvent  from "./components/makeEvent";
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
+  return (<MakeEvent/>);
+  // const [currentForm, setCurrentForm] = useState('login');
 
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
+  // const toggleForm = (formName) => {
+  //   setCurrentForm(formName);
+  // }
 
-  return (
-    <div className="App">
-      {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
-      }
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     {
+  //       currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+  //     }
+  //   </div>
+  // );
 }
 
 export default App;
